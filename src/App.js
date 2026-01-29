6b23b1681f3583d33363ec78f7047d10fd37495b
姜源 <yuan62387@qq.com>
2026-01-29 12:49:56 +0800

    搭建路由

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react'
-import Child from './Child'
+// import React, { useEffect } from 'react'
+// import Child from './Child'
 import './App.css'
-import axios from 'axios'
+// import axios from 'axios'
 import IndexRouter from './router/IndexRouter'
 
 export default function App() {
-
   // 测试验证 反向代理 - 可删除
   // useEffect(() => {
   //   axios("/ajax/comingList?ci=73&token=&limit=10&optimus_risk_level=71&optimus_code=10")
--- src/views/sandbox/NewsSandBox.js
+++ src/views/sandbox/NewsSandBox.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import SideMenu from '../../components/SideMenu'
+import TopHeader from '../../components/TopHeader'
+import Home from './home/Home'
+import RightList from './right-manage/RightList'
+import RoleList from './right-manage/RoleList'
+import UserList from './user-manage/UserList'
+import NoPermission from './nopermission/NoPermission'
 
+// 登陆后的首页 /
 export default function NewsSandBox() {
   return (
-    <div>NewsSandBox</div>
+    <div>
+      <SideMenu></SideMenu>
+      <TopHeader></TopHeader>
+      {/* 放路由组件，切换不同的内容 */}
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/user-manage/list" component={UserList} />
+        <Route path="/right-manage/role/list" component={RoleList} />
+        <Route path="/right-manage/right/list" component={RightList} />
+
+        <Redirect from="/" to="/home" exact /> {/* exact 精确匹配 */}
+        <Route path="*" component={NoPermission} /> {/* 404 兜底配置 */}
+      </Switch>
+    </div>
   )
 }
